90158a3fd14a7907ba1ee81359ceec0fc6f7d691
Gauraang P. Kelkar <gauraangkelkar@gmail.com>
2021-01-18 19:33:10 -0500

    First Commit

---
+++ FinalExam_Program_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FinalExam_Program_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailBox box = new MailBox();
+            MailNotifier notifier = new MailNotifier("Gauraang");
+            notifier += box.receiveMail(box, notifier);
+
+        }
+    }
+
+    class MailBox
+    {
+        public void receiveMail(object s, MailNotificationArgs args)
+        {
+            Console.WriteLine("Mail received from " + args._name);
+        }
+    }
+
+    class MailNotifier
+    {
+        public event EventHandler<MailNotificationArgs> notify;
+
+        public MailNotifier(String name)
+        {
+            if (notify != null)
+            {
+                notify(this, new MailNotificationArgs(name));
+            }
+        }
+    }
+
+    public class MailNotificationArgs : EventArgs
+    {
+        public String _name;
+        public MailNotificationArgs(String name)
+        {
+            this._name = name;
+        }
+        
+    }
+}
